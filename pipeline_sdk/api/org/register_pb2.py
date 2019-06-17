5fcba9d49999b1008677fdb92f737a52e861d6e5
alrenhuang <alrenhuang@easyops.cn>
2019-06-17 19:33:51 +0800

    chore(models): update models

--- pipeline_sdk/api/org/register_pb2.py
+++ pipeline_sdk/api/org/register_pb2.py
@@ -12,6 +12,7 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
+from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
@@ -19,55 +20,11 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='org',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x0eregister.proto\x12\x03org\"@\n\x10RegisterResponse\x12\x0c\n\x04\x63ode\x18\x01 \x01(\x05\x12\r\n\x05\x65rror\x18\x02 \x01(\t\x12\x0f\n\x07message\x18\x03 \x01(\t\"p\n\x17RegisterResponseWrapper\x12\x0c\n\x04\x63ode\x18\x01 \x01(\x05\x12\x13\n\x0b\x63odeExplain\x18\x02 \x01(\t\x12\r\n\x05\x65rror\x18\x03 \x01(\t\x12#\n\x04\x64\x61ta\x18\x04 \x01(\x0b\x32\x15.org.RegisterResponseb\x06proto3')
-)
+  serialized_pb=_b('\n\x0eregister.proto\x12\x03org\x1a\x1bgoogle/protobuf/empty.proto\"q\n\x17RegisterResponseWrapper\x12\x0c\n\x04\x63ode\x18\x01 \x01(\x05\x12\x13\n\x0b\x63odeExplain\x18\x02 \x01(\t\x12\r\n\x05\x65rror\x18\x03 \x01(\t\x12$\n\x04\x64\x61ta\x18\x04 \x01(\x0b\x32\x16.google.protobuf.Emptyb\x06proto3')
+  ,
+  dependencies=[google_dot_protobuf_dot_empty__pb2.DESCRIPTOR,])
 
 
-
-
-_REGISTERRESPONSE = _descriptor.Descriptor(
-  name='RegisterResponse',
-  full_name='org.RegisterResponse',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='code', full_name='org.RegisterResponse.code', index=0,
-      number=1, type=5, cpp_type=1, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR),
-    _descriptor.FieldDescriptor(
-      name='error', full_name='org.RegisterResponse.error', index=1,
-      number=2, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR),
-    _descriptor.FieldDescriptor(
-      name='message', full_name='org.RegisterResponse.message', index=2,
-      number=3, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=23,
-  serialized_end=87,
-)
 
 
 _REGISTERRESPONSEWRAPPER = _descriptor.Descriptor(
@@ -117,21 +74,13 @@ _REGISTERRESPONSEWRAPPER = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=89,
-  serialized_end=201,
+  serialized_start=52,
+  serialized_end=165,
 )
 
-_REGISTERRESPONSEWRAPPER.fields_by_name['data'].message_type = _REGISTERRESPONSE
-DESCRIPTOR.message_types_by_name['RegisterResponse'] = _REGISTERRESPONSE
+_REGISTERRESPONSEWRAPPER.fields_by_name['data'].message_type = google_dot_protobuf_dot_empty__pb2._EMPTY
 DESCRIPTOR.message_types_by_name['RegisterResponseWrapper'] = _REGISTERRESPONSEWRAPPER
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
-
-RegisterResponse = _reflection.GeneratedProtocolMessageType('RegisterResponse', (_message.Message,), dict(
-  DESCRIPTOR = _REGISTERRESPONSE,
-  __module__ = 'register_pb2'
-  # @@protoc_insertion_point(class_scope:org.RegisterResponse)
-  ))
-_sym_db.RegisterMessage(RegisterResponse)
 
 RegisterResponseWrapper = _reflection.GeneratedProtocolMessageType('RegisterResponseWrapper', (_message.Message,), dict(
   DESCRIPTOR = _REGISTERRESPONSEWRAPPER,
