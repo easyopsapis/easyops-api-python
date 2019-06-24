7234566c2def2c1657a9060b69fcc755a9c1b713
alrenhuang <alrenhuang@easyops.cn>
2019-06-24 16:10:15 +0800

    chore(models): update models

--- resource_manage_sdk/client.py
+++ resource_manage_sdk/client.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import api.cmdb_approve.cmdb_approve_client
+
 import api.service_manage.service_manage_client
 
 
 
 class Client(object):
     def __init__(self, server_ip="", server_port=0, service_name=""):
+        
+        self.cmdb_approve = api.cmdb_approve.cmdb_approve_client.CmdbApproveClient(server_ip, server_port, service_name)
         
         self.service_manage = api.service_manage.service_manage_client.ServiceManageClient(server_ip, server_port, service_name)
         
