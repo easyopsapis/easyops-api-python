65287341ab615850e90131d25711888771ba0b00
alrenhuang <alrenhuang@easyops.cn>
2019-06-28 20:23:15 +0800

    chore(models): update models

--- pipeline_sdk/api/pipeline/update_trigger_pb2.py
+++ pipeline_sdk/api/pipeline/update_trigger_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='pipeline',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x14update_trigger.proto\x12\x08pipeline\x1a\x1cmodel/pipeline/trigger.proto\"o\n\x14UpdateTriggerRequest\x12\n\n\x02id\x18\x01 \x01(\t\x12\x12\n\nproject_id\x18\x02 \x01(\t\x12\x13\n\x0bpipeline_id\x18\x03 \x01(\t\x12\"\n\x07trigger\x18\x04 \x01(\x0b\x32\x11.pipeline.Trigger\"q\n\x1cUpdateTriggerResponseWrapper\x12\x0c\n\x04\x63ode\x18\x01 \x01(\x05\x12\x13\n\x0b\x63odeExplain\x18\x02 \x01(\t\x12\r\n\x05\x65rror\x18\x03 \x01(\t\x12\x1f\n\x04\x64\x61ta\x18\x04 \x01(\x0b\x32\x11.pipeline.Triggerb\x06proto3')
+  serialized_pb=_b('\n\x14update_trigger.proto\x12\x08pipeline\x1a\x1cmodel/pipeline/trigger.proto\"Z\n\x14UpdateTriggerRequest\x12\n\n\x02id\x18\x01 \x01(\t\x12\x12\n\nproject_id\x18\x02 \x01(\t\x12\"\n\x07trigger\x18\x03 \x01(\x0b\x32\x11.pipeline.Trigger\"q\n\x1cUpdateTriggerResponseWrapper\x12\x0c\n\x04\x63ode\x18\x01 \x01(\x05\x12\x13\n\x0b\x63odeExplain\x18\x02 \x01(\t\x12\r\n\x05\x65rror\x18\x03 \x01(\t\x12\x1f\n\x04\x64\x61ta\x18\x04 \x01(\x0b\x32\x11.pipeline.Triggerb\x06proto3')
   ,
   dependencies=[model_dot_pipeline_dot_trigger__pb2.DESCRIPTOR,])
 
@@ -49,15 +49,8 @@ _UPDATETRIGGERREQUEST = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='pipeline_id', full_name='pipeline.UpdateTriggerRequest.pipeline_id', index=2,
-      number=3, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR),
-    _descriptor.FieldDescriptor(
-      name='trigger', full_name='pipeline.UpdateTriggerRequest.trigger', index=3,
-      number=4, type=11, cpp_type=10, label=1,
+      name='trigger', full_name='pipeline.UpdateTriggerRequest.trigger', index=2,
+      number=3, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -75,7 +68,7 @@ _UPDATETRIGGERREQUEST = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=64,
-  serialized_end=175,
+  serialized_end=154,
 )
 
 
@@ -126,8 +119,8 @@ _UPDATETRIGGERRESPONSEWRAPPER = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=177,
-  serialized_end=290,
+  serialized_start=156,
+  serialized_end=269,
 )
 
 _UPDATETRIGGERREQUEST.fields_by_name['trigger'].message_type = model_dot_pipeline_dot_trigger__pb2._TRIGGER
