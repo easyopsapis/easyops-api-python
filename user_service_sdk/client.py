ddea5ac85d984fc840eccb190b2f22260d88e7aa
alrenhuang <alrenhuang@easyops.cn>
2019-07-01 11:23:07 +0800

    chore(models): update models

--- user_service_sdk/client.py
+++ user_service_sdk/client.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import api.apikey.apikey_client
+
 import api.invitation_code.invitation_code_client
 
 import api.user_admin.user_admin_client
@@ -8,6 +10,8 @@ import api.user_admin.user_admin_client
 
 class Client(object):
     def __init__(self, server_ip="", server_port=0, service_name=""):
+        
+        self.apikey = api.apikey.apikey_client.ApikeyClient(server_ip, server_port, service_name)
         
         self.invitation_code = api.invitation_code.invitation_code_client.InvitationCodeClient(server_ip, server_port, service_name)
         
