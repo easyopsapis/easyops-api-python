5dbb65f53ce72ec1d00ef949b40a2f2978e6aa15
alrenhuang <alrenhuang@easyops.cn>
2019-07-02 09:52:57 +0800

    chore(models): update models

--- user_service_sdk/client.py
+++ user_service_sdk/client.py
@@ -4,6 +4,8 @@ import api.apikey.apikey_client
 
 import api.invitation_code.invitation_code_client
 
+import api.mongo.mongo_client
+
 import api.user_admin.user_admin_client
 
 
@@ -14,6 +16,8 @@ class Client(object):
         self.apikey = api.apikey.apikey_client.ApikeyClient(server_ip, server_port, service_name)
         
         self.invitation_code = api.invitation_code.invitation_code_client.InvitationCodeClient(server_ip, server_port, service_name)
+        
+        self.mongo = api.mongo.mongo_client.MongoClient(server_ip, server_port, service_name)
         
         self.user_admin = api.user_admin.user_admin_client.UserAdminClient(server_ip, server_port, service_name)
         
