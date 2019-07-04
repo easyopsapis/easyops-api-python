65c08681cb65d7c1077fbab1e697351696463ed4
alrenhuang <alrenhuang@easyops.cn>
2019-07-04 12:03:56 +0800

    chore(models): update models

--- notify_sdk/client.py
+++ notify_sdk/client.py
@@ -2,10 +2,14 @@
 
 import api.oplog.oplog_client
 
+import api.subscriber.subscriber_client
+
 
 
 class Client(object):
     def __init__(self, server_ip="", server_port=0, service_name=""):
         
         self.oplog = api.oplog.oplog_client.OplogClient(server_ip, server_port, service_name)
+        
+        self.subscriber = api.subscriber.subscriber_client.SubscriberClient(server_ip, server_port, service_name)
         
