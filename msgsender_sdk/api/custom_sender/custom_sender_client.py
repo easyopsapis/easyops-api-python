0966223912be0208b3552e72132925d585929b0a
server109 <service@easyops.cn>
2019-07-05 16:38:51 +0800

    chore(models): update models

---
+++ msgsender_sdk/api/custom_sender/custom_sender_client.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+
+import google.protobuf.empty_pb2
+
+import model.msgsender.support_inform_pb2
+
+import model.msgsender.send_message_request_pb2
+
+import model.msgsender.send_message_response_pb2
+
+import model.msgsender.send_message_for_alert_request_pb2
+
+import model.msgsender.send_message_with_appendix_request_pb2
+
+import utils.http_util
+import google.protobuf.json_format
+
+
+class CustomSenderClient(object):
+    def __init__(self, server_ip="", server_port=0, service_name="", host=""):
+        """
+        初始化client
+        :param server_ip: 指定sdk请求的server_ip，为空时走名字服务路由
+        :param server_port: 指定sdk请求的server_port，与server_ip一起使用, 为空时走名字服务路由
+        :param service_name: 指定sdk请求的service_name, 为空时按契约名称路由。如果server_ip和service_name同时设置，server_ip优先级更高
+        :param host: 指定sdk请求服务的host名称, 如cmdb.easyops-only.com
+        """
+        if server_ip == "" and server_port != 0 or server_ip != "" and server_port == 0:
+            raise Exception("server_ip和server_port必须同时指定")
+        self._server_ip = server_ip
+        self._server_port = server_port
+        self._service_name = service_name
+        self._host = host
+
+    
+    def list_support_inform(self, request, org, user, timeout=10):
+        # type: (google.protobuf.empty_pb2.Empty, int, str, int) -> model.msgsender.support_inform_pb2.SupportInform
+        """
+        获取消息通道目前支持的通知方式
+        :param request: list_support_inform请求
+        :param org: 客户的org编号，为数字
+        :param user: 调用api使用的用户名
+        :param timeout: 调用超时时间，单位秒
+        :return: model.msgsender.support_inform_pb2.SupportInform
+        """
+        headers = {"org": org, "user": user}
+        route_name = ""
+        server_ip = self._server_ip
+        if self._service_name != "":
+            route_name = self._service_name
+        elif self._server_ip != "":
+            route_name = "easyops.api.msgsender.custom_sender.ListSupportInform"
+        uri = "/api/v1/message_sender/method"
+        
+        requestParam = request
+        
+        rsp_obj = utils.http_util.do_api_request(
+            method="GET",
+            src_name="logic.msgsender_sdk",
+            dst_name=route_name,
+            server_ip=server_ip,
+            server_port=self._server_port,
+            host=self._host,
+            uri=uri,
+            params=google.protobuf.json_format.MessageToDict(
+                requestParam, preserving_proto_field_name=True),
+            headers=headers,
+            timeout=timeout,
+        )
+        rsp = model.msgsender.support_inform_pb2.SupportInform()
+        
+        google.protobuf.json_format.ParseDict(rsp_obj["data"], rsp, ignore_unknown_fields=True)
+        
+        return rsp
+    
+    def send_message(self, request, org, user, timeout=10):
+        # type: (model.msgsender.send_message_request_pb2.SendMessageRequest, int, str, int) -> model.msgsender.send_message_response_pb2.SendMessageResponse
+        """
+        发送通知消息
+        :param request: send_message请求
+        :param org: 客户的org编号，为数字
+        :param user: 调用api使用的用户名
+        :param timeout: 调用超时时间，单位秒
+        :return: model.msgsender.send_message_response_pb2.SendMessageResponse
+        """
+        headers = {"org": org, "user": user}
+        route_name = ""
+        server_ip = self._server_ip
+        if self._service_name != "":
+            route_name = self._service_name
+        elif self._server_ip != "":
+            route_name = "easyops.api.msgsender.custom_sender.SendMessage"
+        uri = "/api/v1/message_sender/send_message"
+        
+        requestParam = request
+        
+        rsp_obj = utils.http_util.do_api_request(
+            method="POST",
+            src_name="logic.msgsender_sdk",
+            dst_name=route_name,
+            server_ip=server_ip,
+            server_port=self._server_port,
+            host=self._host,
+            uri=uri,
+            params=google.protobuf.json_format.MessageToDict(
+                requestParam, preserving_proto_field_name=True),
+            headers=headers,
+            timeout=timeout,
+        )
+        rsp = model.msgsender.send_message_response_pb2.SendMessageResponse()
+        
+        google.protobuf.json_format.ParseDict(rsp_obj, rsp, ignore_unknown_fields=True)
+        
+        return rsp
+    
+    def send_message_for_alert(self, request, org, user, timeout=10):
+        # type: (model.msgsender.send_message_for_alert_request_pb2.SendMessageForAlertRequest, int, str, int) -> model.msgsender.send_message_response_pb2.SendMessageResponse
+        """
+        处理告警通知消息
+        :param request: send_message_for_alert请求
+        :param org: 客户的org编号，为数字
+        :param user: 调用api使用的用户名
+        :param timeout: 调用超时时间，单位秒
+        :return: model.msgsender.send_message_response_pb2.SendMessageResponse
+        """
+        headers = {"org": org, "user": user}
+        route_name = ""
+        server_ip = self._server_ip
+        if self._service_name != "":
+            route_name = self._service_name
+        elif self._server_ip != "":
+            route_name = "easyops.api.msgsender.custom_sender.SendMessageForAlert"
+        uri = "/api/v1/alert_adapter/receive"
+        
+        requestParam = request
+        
+        rsp_obj = utils.http_util.do_api_request(
+            method="POST",
+            src_name="logic.msgsender_sdk",
+            dst_name=route_name,
+            server_ip=server_ip,
+            server_port=self._server_port,
+            host=self._host,
+            uri=uri,
+            params=google.protobuf.json_format.MessageToDict(
+                requestParam, preserving_proto_field_name=True),
+            headers=headers,
+            timeout=timeout,
+        )
+        rsp = model.msgsender.send_message_response_pb2.SendMessageResponse()
+        
+        google.protobuf.json_format.ParseDict(rsp_obj, rsp, ignore_unknown_fields=True)
+        
+        return rsp
+    
+    def send_message_with_appendix(self, request, org, user, timeout=10):
+        # type: (model.msgsender.send_message_with_appendix_request_pb2.SendMessageWithAppendixRequest, int, str, int) -> model.msgsender.send_message_response_pb2.SendMessageResponse
+        """
+        处理带附件的通知消息
+        :param request: send_message_with_appendix请求
+        :param org: 客户的org编号，为数字
+        :param user: 调用api使用的用户名
+        :param timeout: 调用超时时间，单位秒
+        :return: model.msgsender.send_message_response_pb2.SendMessageResponse
+        """
+        headers = {"org": org, "user": user}
+        route_name = ""
+        server_ip = self._server_ip
+        if self._service_name != "":
+            route_name = self._service_name
+        elif self._server_ip != "":
+            route_name = "easyops.api.msgsender.custom_sender.SendMessageWithAppendix"
+        uri = "/api/v1/message_sender/mail_with_appendix"
+        
+        requestParam = request
+        
+        rsp_obj = utils.http_util.do_api_request(
+            method="POST",
+            src_name="logic.msgsender_sdk",
+            dst_name=route_name,
+            server_ip=server_ip,
+            server_port=self._server_port,
+            host=self._host,
+            uri=uri,
+            params=google.protobuf.json_format.MessageToDict(
+                requestParam, preserving_proto_field_name=True),
+            headers=headers,
+            timeout=timeout,
+        )
+        rsp = model.msgsender.send_message_response_pb2.SendMessageResponse()
+        
+        google.protobuf.json_format.ParseDict(rsp_obj, rsp, ignore_unknown_fields=True)
+        
+        return rsp
+    
