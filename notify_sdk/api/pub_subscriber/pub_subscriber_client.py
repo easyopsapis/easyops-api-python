f41fd3ec3b1499317f9abe146bb9d6d0340e0e9f
server109 <service@easyops.cn>
2019-07-25 14:52:20 +0800

    chore(models): update models

---
+++ notify_sdk/api/pub_subscriber/pub_subscriber_client.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+PROJECT_PATH = os.path.dirname(os.path.dirname(current_path))
+if PROJECT_PATH not in sys.path:
+    sys.path.append(PROJECT_PATH)
+
+
+import model.notify.pub_subscriber_pb2
+
+import utils.http_util
+import google.protobuf.json_format
+
+
+class PubSubscriberClient(object):
+    def __init__(self, server_ip="", server_port=0, service_name="", host=""):
+        """
+        初始化client
+        :param server_ip: 指定sdk请求的server_ip，为空时走名字服务路由
+        :param server_port: 指定sdk请求的server_port，与server_ip一起使用, 为空时走名字服务路由
+        :param service_name: 指定sdk请求的service_name, 为空时按契约名称路由。如果server_ip和service_name同时设置，server_ip优先级更高
+        :param host: 指定sdk请求服务的host名称, 如cmdb.easyops-only.com
+        """
+        if server_ip == "" and server_port != 0 or server_ip != "" and server_port == 0:
+            raise Exception("server_ip和server_port必须同时指定")
+        self._server_ip = server_ip
+        self._server_port = server_port
+        self._service_name = service_name
+        self._host = host
+
+    
+    def pub_subscriber_create(self, request, org, user, timeout=10):
+        # type: (model.notify.pub_subscriber_pb2.PubSubscriber, int, str, int) -> model.notify.pub_subscriber_pb2.PubSubscriber
+        """
+        添加订阅者
+        :param request: pub_subscriber_create请求
+        :param org: 客户的org编号，为数字
+        :param user: 调用api使用的用户名
+        :param timeout: 调用超时时间，单位秒
+        :return: model.notify.pub_subscriber_pb2.PubSubscriber
+        """
+        headers = {"org": org, "user": user}
+        route_name = ""
+        server_ip = self._server_ip
+        if self._service_name != "":
+            route_name = self._service_name
+        elif self._server_ip != "":
+            route_name = "easyops.api.notify.pub_subscriber.PubSubscriberCreate"
+        uri = "/pub_subscriber/create"
+        
+        requestParam = request
+        
+        rsp_obj = utils.http_util.do_api_request(
+            method="POST",
+            src_name="logic.notify_sdk",
+            dst_name=route_name,
+            server_ip=server_ip,
+            server_port=self._server_port,
+            host=self._host,
+            uri=uri,
+            params=google.protobuf.json_format.MessageToDict(
+                requestParam, preserving_proto_field_name=True),
+            headers=headers,
+            timeout=timeout,
+        )
+        rsp = model.notify.pub_subscriber_pb2.PubSubscriber()
+        
+        google.protobuf.json_format.ParseDict(rsp_obj["data"], rsp, ignore_unknown_fields=True)
+        
+        return rsp
+    
+    def pub_subscriber_update(self, request, org, user, timeout=10):
+        # type: (model.notify.pub_subscriber_pb2.PubSubscriber, int, str, int) -> model.notify.pub_subscriber_pb2.PubSubscriber
+        """
+        更新订阅者
+        :param request: pub_subscriber_update请求
+        :param org: 客户的org编号，为数字
+        :param user: 调用api使用的用户名
+        :param timeout: 调用超时时间，单位秒
+        :return: model.notify.pub_subscriber_pb2.PubSubscriber
+        """
+        headers = {"org": org, "user": user}
+        route_name = ""
+        server_ip = self._server_ip
+        if self._service_name != "":
+            route_name = self._service_name
+        elif self._server_ip != "":
+            route_name = "easyops.api.notify.pub_subscriber.PubSubscriberUpdate"
+        uri = "/pub_subscriber/update"
+        
+        requestParam = request
+        
+        rsp_obj = utils.http_util.do_api_request(
+            method="POST",
+            src_name="logic.notify_sdk",
+            dst_name=route_name,
+            server_ip=server_ip,
+            server_port=self._server_port,
+            host=self._host,
+            uri=uri,
+            params=google.protobuf.json_format.MessageToDict(
+                requestParam, preserving_proto_field_name=True),
+            headers=headers,
+            timeout=timeout,
+        )
+        rsp = model.notify.pub_subscriber_pb2.PubSubscriber()
+        
+        google.protobuf.json_format.ParseDict(rsp_obj["data"], rsp, ignore_unknown_fields=True)
+        
+        return rsp
+    
--- notify_sdk/client.py
+++ notify_sdk/client.py
@@ -2,6 +2,8 @@
 
 import api.oplog.oplog_client
 
+import api.pub_subscriber.pub_subscriber_client
+
 import api.subscriber.subscriber_client
 
 
@@ -10,6 +12,8 @@ class Client(object):
     def __init__(self, server_ip="", server_port=0, service_name=""):
         
         self.oplog = api.oplog.oplog_client.OplogClient(server_ip, server_port, service_name)
+        
+        self.pub_subscriber = api.pub_subscriber.pub_subscriber_client.PubSubscriberClient(server_ip, server_port, service_name)
         
         self.subscriber = api.subscriber.subscriber_client.SubscriberClient(server_ip, server_port, service_name)
         
